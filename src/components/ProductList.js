108f8db0c7ef976a64d89ad1fa401df414bf110f
TarannumParwin <105907591+TarannumParwin@users.noreply.github.com>
2023-08-31 19:43:01 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,55 +1,45 @@
-import React, { useState } from 'react';
-import Product from './Product';
+import React, { useState } from "react";
+import Product from "./Product";
 
 const ProductList = ({ products, addToCart }) => {
-  const [sortOption, setSortOption] = useState('default');
+  const [sortOption, setSortOption] = useState("default");
 
-  const handleSortChange = e => {
+  const handleSortChange = (e) => {
     setSortOption(e.target.value);
   };
 
   const sortedProducts = [...products];
 
-  if (sortOption === 'priceLowToHigh') {
+  if (sortOption === "priceLowToHigh") {
     sortedProducts.sort((a, b) => a.price - b.price);
-  } else if (sortOption === 'priceHighToLow') {
+  } else if (sortOption === "priceHighToLow") {
     sortedProducts.sort((a, b) => b.price - a.price);
-  } else if (sortOption === 'ratings') {
+  } else if (sortOption === "ratings") {
     sortedProducts.sort((a, b) => b.ratings - a.ratings);
   }
 
   return (
-    <div className="product-list">
-      
-      <div style={styles.products}>
-        {sortedProducts.map(product => (
+    <div>
+      <div className="header">
+        <h2>Product List</h2>
+        <div className="filter">
+          <label className="label">Sort by:</label>
+          <select className="select" onChange={handleSortChange}>
+            <option value="default">Default</option>
+            <option value="priceLowToHigh">Price: Low to High</option>
+            <option value="priceHighToLow">Price: High to Low</option>
+            <option value="ratings">Ratings</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="products">
+        {sortedProducts.map((product) => (
           <Product key={product.id} product={product} addToCart={addToCart} />
         ))}
       </div>
     </div>
   );
-};
-
-const styles = {
-  filter: {
-    marginBottom: '20px',
-    display: 'flex',
-    alignItems: 'center',
-  },
-  label: {
-    marginRight: '10px',
-  },
-  select: {
-    padding: '8px',
-    border: '1px solid #ddd',
-    borderRadius: '4px',
-    cursor: 'pointer',
-  },
-  products: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(220px, 1fr))',
-    gap: '20px',
-  },
 };
 
 export default ProductList;
