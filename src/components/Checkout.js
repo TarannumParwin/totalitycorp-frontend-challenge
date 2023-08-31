60cb3aab4289b3497858454a0da031d3f574dd8f
TarannumParwin <105907591+TarannumParwin@users.noreply.github.com>
2023-08-31 19:36:48 +0530

    Update Checkout.js

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,34 +1,33 @@
-
-
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Checkout = ({ cartItems, total, onClose }) => {
   const [shippingInfo, setShippingInfo] = useState({});
   const [paymentInfo, setPaymentInfo] = useState({});
+  const [orderSuccess, setOrderSuccess] = useState(false);
 
-  const handleShippingChange = e => {
+  const handleShippingChange = (e) => {
     const { name, value } = e.target;
-    setShippingInfo(prevShippingInfo => ({
+    setShippingInfo((prevShippingInfo) => ({
       ...prevShippingInfo,
-      [name]: value
+      [name]: value,
     }));
   };
 
-  const handlePaymentChange = e => {
+  const handlePaymentChange = (e) => {
     const { name, value } = e.target;
-    setPaymentInfo(prevPaymentInfo => ({
+    setPaymentInfo((prevPaymentInfo) => ({
       ...prevPaymentInfo,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const handleSubmit = () => {
     // Handle form submission
-    console.log('Shipping Information:', shippingInfo);
-    console.log('Payment Information:', paymentInfo);
-    console.log('Cart Items:', cartItems);
-    console.log('Total:', total);
+    console.log("Shipping Information:", shippingInfo);
+    console.log("Payment Information:", paymentInfo);
+    console.log("Cart Items:", cartItems);
+    console.log("Total:", total);
+    setOrderSuccess(true); // Set order success state to true
     onClose(); // Close the modal after submission
   };
 
@@ -43,35 +42,59 @@ const Checkout = ({ cartItems, total, onClose }) => {
           <div className="shipping-info">
             <h3>Shipping Information</h3>
             <form>
-              <label htmlFor="fullName">Full Name</label>
-              <input type="text" id="fullName" name="fullName" onChange={handleShippingChange} required />
-              
-             
-
-          <label htmlFor="address">Address</label>
-          <input type="text" id="address" name="address" onChange={handleShippingChange} required />
-
-        
-
-          
-              
+              <div>
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                  type="text"
+                  id="fullName"
+                  name="fullName"
+                  onChange={handleShippingChange}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  id="address"
+                  name="address"
+                  onChange={handleShippingChange}
+                  required
+                />
+              </div>
             </form>
           </div>
           <div className="payment-info">
             <h3>Payment Information</h3>
             <form>
-              <label htmlFor="cardNumber">Card Number</label>
-              <input type="text" id="cardNumber" name="cardNumber" onChange={handlePaymentChange} required />
-              
-              
-
-        
-          <label htmlFor="cvv">CVV</label>
-          <input type="text" id="cvv" name="cvv" onChange={handlePaymentChange} required />
-              
+              <div>
+                <label htmlFor="cardNumber">Card Number</label>
+                <input
+                  type="text"
+                  id="cardNumber"
+                  name="cardNumber"
+                  onChange={handlePaymentChange}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="cvv">CVV</label>
+                <input
+                  type="text"
+                  id="cvv"
+                  name="cvv"
+                  onChange={handlePaymentChange}
+                  required
+                />
+              </div>
             </form>
           </div>
-          <button className="place-order-button" onClick={handleSubmit}>Place Order</button>
+          <button className="place-order-button" onClick={handleSubmit}>
+            Place Order
+          </button>
+          {orderSuccess && (
+            <p className="order-success-message">Ordered successfully!</p>
+          )}
         </div>
       </div>
     </div>
